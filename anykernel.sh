c07fa67b785d62ebf654ed75519fd6153de4bd30
Senseii <princekarlagas@gmail.com>
2025-01-30 20:54:06 +0800

    anyk3: rework anyk3 and major debloat
    refining anykernel.sh to make renaming method work properly. also debloated it

---
+++ anykernel.sh
@@ -0,0 +1,108 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=FusionX by SenX (@WazzupSensei911) --Telegram
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=munch
+device.name2=munchin
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+# kernel naming scene
+ui_print " ";
+ui_print "╔══════════════════════════════════════════╗";
+ui_print "║        F U S I O N X  K E R N E L        ║";
+ui_print "║           Powering your Device           ║";
+ui_print "╚══════════════════════════════════════════╝";
+ui_print " ";
+
+case "$ZIPFILE" in
+  *miui*|*MIUI*)
+    ui_print "MIUI/HyperOS variant identified,";
+    ui_print "---> Applying MIUI DTBO... ";
+    mv *-miui-dtbo.img $home/dtbo.img;
+    rm *-aosp-dtbo.img;
+  ;;
+  *aosp*|*AOSP*)
+    ui_print "AOSP variant identified,";
+    ui_print "---> Applying AOSP DTBO... ";
+    mv *-aosp-dtbo.img $home/dtbo.img;
+    rm *-miui-dtbo.img;
+  ;;
+  *)
+    ui_print "No specific variant detected !!!";
+    ui_print "---> Applying AOSP DTBO... ";
+    mv *-aosp-dtbo.img $home/dtbo.img;
+    rm *-miui-dtbo.img;
+  ;;
+esac
+ui_print " ";
+
+case "$ZIPFILE" in
+  *eff*|*EFF*)
+    ui_print "Efficient CPU variant identified,";
+    ui_print "---> Applying Efficient CPU variant...";
+    mv *-effcpu-dtb $home/dtb;
+    rm *-normal-dtb;
+  ;;
+  *)
+    ui_print "Normal CPU variant identified,";
+    ui_print "---> Applying Normal CPU variant...";
+    mv *-normal-dtb $home/dtb;
+    rm *-effcpu-dtb;
+  ;;
+esac
+
+## AnyKernel install
+dump_boot;
+
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+write_boot;
+## end install
+
+## vendor_boot shell variables
+block=/dev/block/bootdevice/by-name/vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+reset_ak;
+
+# vendor_boot install
+dump_boot;
+
+write_boot;
+## end vendor_boot install
+
