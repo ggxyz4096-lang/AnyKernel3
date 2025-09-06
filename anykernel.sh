3ba24bc2097b778d4a15ee5640a44baeeb8477c3
ggxyz4096-lang <ggxyz4096@gmail.com>
2025-09-06 17:52:15 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -27,7 +27,8 @@ mv $home/kernels/dtbo.img $home/dtbo.img;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 750 750 $ramdisk/*;
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel install
 dump_boot;
